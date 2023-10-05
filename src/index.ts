a57c5341067bebeac5763afc32c7790e57b01fc2
ttahm3d <ttahm3d@gmail.com>
2023-10-05 20:42:09 +0530

    [feat] - added middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import * as dotenv from "dotenv";
 dotenv.config();
 import { connectToDb } from "./db/connect";
 
 const app = express();
-const port = 3001 || process.env.PORT;
+const port: number = parseInt(process.env.PORT);
 
+// Connect to the database
 connectToDb();
+
+// Initialize Middlewares
+
+// Cross origin resouce sharing - defines how resources defined in one domain can be used by other domains
+// Empty means can be accessed by anyone
+app.use(cors());
+
+// Secures an app by setting response headers
+app.use(helmet());
+
+// Allows app to parse JSON data
+app.use(express.json());
+
+// Parses URL Encoded data and provides it in req.body
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Main Route reverted");
 });
 
 app.listen(port, () => {
-  console.log(`[index.js]: App running on port ${port}`);
+  console.log(`[index.ts]: App running on port ${port}`);
 });
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       DB_CONNECTION_STRING: string;
+      PORT: string;
     }
   }
 }
