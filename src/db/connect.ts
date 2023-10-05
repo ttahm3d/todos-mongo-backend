06369621aa94c6f0c7664b7ca8bc0652c18f6411
ttahm3d <ttahm3d@gmail.com>
2023-10-05 20:28:03 +0530

    [feat] - configured enivonment variables

---
+++ src/db/connect.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectToDb = async () => {
+  try {
+    const connectionString = process.env.DB_CONNECTION_STRING!;
+    await mongoose.connect(connectionString, {});
+    console.log("[connect.ts] Connected to database");
+  } catch (error) {
+    console.error("[connect] Could not connect to Database", error);
+  }
+};
+
+export { connectToDb };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+import { connectToDb } from "./db/connect";
 
 const app = express();
 const port = 3001 || process.env.PORT;
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Main Route reverted");
