84e6f096389951e6c39deb4ed95429cec0ec21e0
ttahm3d <ttahm3d@gmail.com>
2023-10-10 19:04:07 +0530

    [feat] - updated model to include timestamps

--- src/models/Todo/Todo.Model.ts
+++ src/models/Todo/Todo.Model.ts
@@ -1,18 +1,21 @@
 import mongoose, { Schema } from "mongoose";
 import { ITodo } from "@/types/schema";
 
-const TodoSchema: Schema<ITodo> = new Schema({
-  title: {
-    type: String,
-    required: [true, "'title' is required to create a todo"],
+const TodoSchema: Schema<ITodo> = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "'title' is required to create a todo"],
+    },
+    description: {
+      type: String,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  description: {
-    type: String,
-  },
-  isCompleted: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("TodoModel", TodoSchema);
+export default mongoose.model<ITodo>("TodoModel", TodoSchema);
