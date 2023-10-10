7cb6d8cc3f923d7e2e1786c82325583c6e36f627
ttahm3d <ttahm3d@gmail.com>
2023-10-10 07:32:44 +0530

    [feat] - added GET by id

--- src/controllers/Todo/Todo.controllers.ts
+++ src/controllers/Todo/Todo.controllers.ts
@@ -17,3 +17,23 @@ export const getTodos = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getTodoById = async (req: Request, res: Response) => {
+  const todoId: string = req.params.id;
+  if (!todoId) res.send(400).json({ error: "Missing parameter 'id'" });
+  try {
+    const todo: ITodo | null = await TodoModel.findOne({ _id: todoId });
+    if (!todo)
+      res.send(404).json({ error: `Could not find todo with id: ${todoId}` });
+    res.status(200).send({
+      message: "Successfully fetched todo",
+      todo,
+    });
+  } catch (error) {
+    console.error(["TodoController:GET -> failed to fetch todos"]);
+    res.status(500).send({
+      messsage: "Failed to fetch todos",
+      error,
+    });
+  }
+};
