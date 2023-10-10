7e5806d82f72e71c7eda4e5fa7368eec27720936
ttahm3d <ttahm3d@gmail.com>
2023-10-10 19:01:38 +0530

    [feat]- using environment vars

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -2,15 +2,13 @@ import mongoose from "mongoose";
 
 const connectToDb = async () => {
   try {
-    const connectionString = process.env.DB_CONNECTION_STRING!;
-    await mongoose.connect(connectionString, {});
+    await mongoose.connect(process.env.DB_CONNECTION_STRING!);
     console.log("[connect.ts] Connected to database");
   } catch (error) {
     console.error(
       "[connect] Could not establish a connection with database",
       error
     );
-    console.error("[connect] Could not connect to Database", error);
   }
 };
 
